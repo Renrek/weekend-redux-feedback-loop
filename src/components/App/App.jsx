aaad5978d8616213dd01ffe2e4e1da1a78ab5684
Brad Johansen <brad.d.johansen@gmail.com>
2021-08-19 17:32:55 -0500

    Routes and Initial Base Components added.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+
+//Components
+import Welcome from '../Welcome/Welcome';
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
+import Admin from '../Admin/Admin';
+
 
 function App() {
 
@@ -10,6 +21,29 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+          <Route path="/">
+            <Welcome />
+          </Route>
+          <Route path="/feeling">
+            <Feeling />
+          </Route>
+          <Route path="/understanding">
+            <Understanding />
+          </Route>
+          <Route path="/supported">
+            <Supported />
+          </Route>
+          <Route path="/comments">
+            <Comments />
+          </Route>
+          <Route path="/review">
+            <Review />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            <button>Take Survay</button>
+        </div>
+    )
+}
+
+export default Welcome;
