7a820562ec625bef904dbcf2b16f2cbc89186a19
Brad Johansen <brad.d.johansen@gmail.com>
2021-08-20 15:46:07 -0500

    added Readme todo

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Project Name
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+## Todo before - Sunday
+
+- [ ] Material UI tuning
+- [ ] Code Refactor /Cleanup
+- [ ] Comment Review / Cleanup
+- [ ] Admin Page
+- [ ] This Read Me....
--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import axios from 'axios';
+
+import { Box } from '@material-ui/core';
 
 const Admin = () => {
+
+    const getFeedback = () => {
+        axios.get('/api/survey')
+            .then( response => {
+                
+            })
+            .catch( error => {
+                alert('There was an error fetching information from the server.')
+                console.log('Error on GET', error);
+            });
+    };
+
     return (
-        <div>
+        <Box>
             
-        </div>
+        </Box>
     )
 }
 
